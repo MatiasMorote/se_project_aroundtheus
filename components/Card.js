89aef3bfe9f33c1dbfa2b554c24a7575b3be05f5
MatiasMorote <morotematias@gmail.com>
2024-11-21 18:45:45 -0500

    started the Card class

--- components/Card.js
+++ components/Card.js
@@ -1,8 +1,37 @@
-export default class Card {
-  constructor(data, cardSelector, handleImageClick) {
-    this._name = data.name;
-    this._link = data.link;
-
+class Card {
+  constructor({ name, link }, cardSelector) {
+    this._name = name;
+    this._link = link;
+    // this._handleImageClick = handleImageClick;
     this._cardSelector = cardSelector;
   }
+
+  _setEventListeners() {
+    //".card__like-button"
+    this._cardElement
+      .querySelector(".card__like-button")
+      .addEventListener("click", () => {
+        this._handleLikeIcon();
+      });
+    //".card__trash-button"
+    const deleteButton = this._cardElement.querySelector(".card__trash-button");
+  }
+
+  _handleLikeIcon() {
+    this._cardElement
+      .querySelector(".card__like-button")
+      .classList.toggle("card__like-button_is-active");
+  }
+
+  getView() {
+    this._cardElement = document
+      .querySelector(this._cardSelector)
+      .content.querySelector(".card")
+      .cloneNode(true);
+    //get the card view
+    //set event listeners
+    this._setEventListeners();
+    //retur the card
+  }
 }
+export default Card;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Card from "../components/Card.js";
 import FormValidator from "../components/FormValidator.js";
 
 const initialCards = [
@@ -26,6 +27,12 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+const cardData = {
+  name: "Yosemite Valley",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+};
+const card = new Card(cardData, "#card-template");
+card.getView();
 
 /* -------------------------------------------------------------------------- */
 /*                                  Elements                                  */
